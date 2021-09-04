71b76f311b08c844310c49d1d7816de5ee1cf9bd
oguzBatur <sariozbatur@gmail.com>
2021-09-04 17:18:52 +0300

    Change Background is Done!

---
+++ app.js
@@ -0,0 +1,13 @@
+const colors = ['red','blue','yellow','green','lightgreen','lightyellow','darkred','lightblue'];
+const body = document.getElementsByTagName('body')[0];
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', () => {
+
+
+    let rndNumber = Math.floor(Math.random() * (colors.length - 1));
+
+    body.style.backgroundColor = colors[rndNumber];
+    console.log(rndNumber)
+})
